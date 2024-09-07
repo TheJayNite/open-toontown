ce93dd70e5dee287f963e0055b5501076ee2500a
John Cote <jwcotejr@gmail.com>
2024-09-07 19:40:39 -0400

    ToontownClient: Handle CLIENT_HEARTBEAT, CLIENT_DISCONNECT

---
+++ otpgo/lua/MsgTypes.lua
@@ -0,0 +1,8 @@
+CLIENT_DISCONNECT =     37
+
+CLIENT_HEARTBEAT =      52
+
+-- new toontown specific login message, adds last logged in, and if child account has parent acount
+CLIENT_LOGIN_TOONTOWN = 125
+
+CLIENT_DISCONNECT_GENERIC = 1
--- otpgo/lua/ToontownClient.lua
+++ otpgo/lua/ToontownClient.lua
@@ -1,6 +1,17 @@
 package.path = package.path .. ";lua/?.lua"
 
+-- Load message types
+dofile("lua/MsgTypes.lua")
+
 function receiveDatagram(client, dgi)
+    -- Client received datagrams
     msgType = dgi:readUint16()
-    print(msgType)
+
+    if msgType == CLIENT_HEARTBEAT then
+        client:handleHeartbeat()
+    elseif msgType == CLIENT_DISCONNECT then
+        client:handleDisconnect()
+    else
+        client:sendDisconnect(CLIENT_DISCONNECT_GENERIC, string.format("Unknown message type: %d", msgType), true)
+    end
 end
