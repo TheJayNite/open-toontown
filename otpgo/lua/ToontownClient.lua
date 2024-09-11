70602f723190bc3ea3e3af6025351e4baa9278e4
John Cote <jwcotejr@gmail.com>
2024-09-10 22:37:56 -0400

    otpgo: Add Utils.lua

--- otpgo/lua/ToontownClient.lua
+++ otpgo/lua/ToontownClient.lua
@@ -3,23 +3,8 @@ package.path = package.path .. ";lua/?.lua"
 local inspect = require('inspect')
 local date = require('date')
 
--- From https://stackoverflow.com/a/22831842
-function string.starts(str, start)
-    return string.sub(str, 1, string.len(start)) == start
-end
-
--- From https://stackoverflow.com/a/2421746
-function string.upperFirst(str)
-    return string.gsub(str, "^%l", string.upper)
-end
-
-function table.shallow_copy(t)
-    local t2 = {}
-    for k, v in pairs(t) do
-        t2[k] = v
-    end
-    return t2
-end
+-- Load Utils:
+dofile("lua/Utils.lua")
 
 -- Read vismap:
 function readVismap()
